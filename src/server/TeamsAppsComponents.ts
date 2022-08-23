cd5f0e59b6fdcb3d158f45cf27e0d96e3862b83a
MONthinkpad <jmontalvo@sanservices.hn>
2022-08-23 09:22:46 -0600

    first commit

---
+++ src/server/TeamsAppsComponents.ts
@@ -0,0 +1,4 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the apmBotOutgoingWebhook outgoing webhook
+export * from "./apmBotOutgoingWebhook/ApmBotOutgoingWebhook";
