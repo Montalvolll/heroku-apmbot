cb53c354825f67c28a0912ee6069fe2ac65ec3c5
Monthinkpad <jose.montalvo@uvltd.tech>
2022-08-30 14:57:34 -0600

    changed the let variable

--- src/server/apmBotOutgoingWebhook/ApmBotOutgoingWebhook.ts
+++ src/server/apmBotOutgoingWebhook/ApmBotOutgoingWebhook.ts
@@ -26,7 +26,7 @@ export class ApmBotOutgoingWebhook implements IOutgoingWebhook {
         const incoming = req.body as builder.Activity;
 
         // create the response, any Teams compatible responses can be used
-        let message: Partial<builder.Activity> = {
+        const message: Partial<builder.Activity> = {
             type: builder.ActivityTypes.Message
         };
 
